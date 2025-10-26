5c0081ac5b23d83b4003ec452a8a9a61f3d7632c
jacobofp2001 <jacobofp2001@gmail.com>
2025-10-26 01:21:13 -0600

    Fix Ubuntu image references: remove deprecated Ubuntu 20.04 and use only Ubuntu 22.04 LTS

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,12 +1,7 @@
 # -----------------------------------------------------------------
-# data.tf  – one place that decides "latest Ubuntu 20.04"
+# data.tf  – one place that decides "latest Ubuntu 22.04"
 # -----------------------------------------------------------------
-data "google_compute_image" "ubuntu_2004" {
-  family  = "ubuntu-2004-lts"
-  project = "ubuntu-os-cloud"
-}
-
-# Since Ubuntu 20.04 is deprecated, let's also add Ubuntu 22.04 as fallback
+# Ubuntu 20.04 LTS is deprecated, so we use Ubuntu 22.04 LTS
 data "google_compute_image" "ubuntu_2204" {
   family  = "ubuntu-2204-lts"
   project = "ubuntu-os-cloud"
